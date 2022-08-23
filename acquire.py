608d1f742e562d9caf9e07e17008ad20ae1b30e1
Vincent Banuelos <vincent.v.banuelos527@gmail.com>
2022-08-22 20:31:05 -0500

    adding acquire file for the telco project

---
+++ acquire.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, user, password
+
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def new_telco_data():
+    '''
+    This function reads the telco data from the Codeup db into a df.
+    '''
+    sql_query = """
+                select * from customers
+                join contract_types using (contract_type_id)
+                join internet_service_types using (internet_service_type_id)
+                join payment_types using (payment_type_id)
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+def get_telco_data():
+    '''
+    This function reads in telco data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('telco.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('telco.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_telco_data()
+        
+        # Cache data
+        df.to_csv('telco.csv')
+        
+    return df
